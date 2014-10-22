7c7665ddb2f81f064061a96348ad43ed20619eb2
9CAT <9cat@users.noreply.github.com>
2014-10-22 13:54:42 -0700

    Update start.sh
    change the general receiving address.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python run_p2pool.py --net templecoin -a TNNmAkFX7oESwVSz3W43PAT8Mcw4GCpn1w --give-author 0 --fee 0 --bitcoind-address localhost --bitcoind-rpc-port 9081 --bitcoind-p2p-port 32581 temple templekingdom -n p2pool.templecoin.org
+python run_p2pool.py --net templecoin -a THQH77urK7kxvsa2d3zJSqf12ByCUwosPB --give-author 0 --fee 0 --bitcoind-address localhost --bitcoind-rpc-port 9081 --bitcoind-p2p-port 32581 temple templekingdom -n p2pool.templecoin.org
 
 
 
