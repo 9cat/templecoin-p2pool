6d17387d687e27916c4c655148a58afc340b19bb
root <root@4c5ce925fd84.(none)>
2014-10-23 23:13:54 +0000

    solve low target can't solve block issue.

--- p2pool/bitcoin/networks/templecoin.py
+++ p2pool/bitcoin/networks/templecoin.py
@@ -16,14 +16,14 @@ RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
             not (yield bitcoind.rpc_getinfo())['testnet']
         ))
 #SUBSIDY_FUNC = lambda height: 1*100000000 >> (height + 1)//840000
-SUBSIDY_FUNC = lambda height: 1
+SUBSIDY_FUNC = lambda height: 1*100000000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
-BLOCK_PERIOD = 150 # s
+BLOCK_PERIOD = 60 # s
 SYMBOL = 'TPC'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Templecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Templecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.templecoin'), 'templecoin.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'http://insight.templecoin.org//block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'http://insight.templecoin.org/address/'
 TX_EXPLORER_URL_PREFIX = 'http://insight.templecoin.org/tx/'
-SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
+SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//100000000 - 1) #default (2**256//1000000000 - 1, 2**256//1000 - 1)
 DUMB_SCRYPT_DIFF = 2**16
 DUST_THRESHOLD = 0.001e8
--- p2pool/networks/templecoin.py
+++ p2pool/networks/templecoin.py
@@ -2,16 +2,16 @@ from p2pool.bitcoin import networks
 
 PARENT = networks.nets['templecoin']
 SHARE_PERIOD = 15 # seconds
-CHAIN_LENGTH = 24*60*60//10 # shares
-REAL_CHAIN_LENGTH = 24*60*60//10 # shares
-TARGET_LOOKBEHIND = 200 # shares
-SPREAD = 3 # blocks
+CHAIN_LENGTH = 12*60*60//15 # shares
+REAL_CHAIN_LENGTH = 12*60*60//15 # shares
+TARGET_LOOKBEHIND = 20 # shares
+SPREAD = 10 # blocks
 IDENTIFIER = 'e03255b8c6923410'.decode('hex')
 PREFIX = '7238c1a53ef629b0'.decode('hex')
 P2P_PORT = 19325
-MIN_TARGET = 0
+MIN_TARGET = 0  #default 0
 MAX_TARGET = 2**256//2**20 - 1
-PERSIST = False
+PERSIST = True
 WORKER_PORT = 19327
 BOOTSTRAP_ADDRS = 'seed.templecoin.org seed.templecoin.com'.split(' ')
 ANNOUNCE_CHANNEL = '#p2pool-tpc'
